9009afefc23670fc011c6d6b1299210dd79837c0
Paul Melton <melton.paul@hotmail.com>
2022-09-16 17:05:23 -0500

    'Similar Song' component + grid styling for Recent

--- src/components/MostRecent/MostRecent.tsx
+++ src/components/MostRecent/MostRecent.tsx
@@ -23,6 +23,14 @@ export default function MostRecent() {
           img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
           name="He Got A Gun"
         />
+        <SmallCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+        />
+        <SmallCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+        />
       </article>
     </section>
   );
--- src/components/SimilarSongs/SimilarSongs.tsx
+++ src/components/SimilarSongs/SimilarSongs.tsx
@@ -1,15 +1,38 @@
 import React from "react";
+import styles from "./SimilarSongs.module.css";
 import MediumCard from "../SongCards/MediumCard/MediumCard";
 
 export default function SimilarSongs() {
   return (
     <section>
       <h2>Similar to what you listen to</h2>
-      <MediumCard
-        img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
-        name="He Got A Gun"
-        desc="New album by Bizarre"
-      />
+      <article className={styles.similarSongs}>
+        <MediumCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+          desc="New album by Bizarre"
+        />
+        <MediumCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+          desc="New album by Bizarre"
+        />
+        <MediumCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+          desc="New album by Bizarre"
+        />
+        <MediumCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+          desc="New album by Bizarre"
+        />
+        <MediumCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+          desc="New album by Bizarre"
+        />
+      </article>
     </section>
   );
 }
