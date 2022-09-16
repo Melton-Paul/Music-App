272ce5c4a80ba378af7ff353d420b36dd95ef751
Paul Melton <melton.paul@hotmail.com>
2022-09-16 16:52:40 -0500

    'Most Recent' component

--- src/components/MostRecent/MostRecent.tsx
+++ src/components/MostRecent/MostRecent.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import styles from "./MostRecent.module.css";
 import SmallCard from "../SongCards/SmallCard/SmallCard";
 
 export default function MostRecent() {
   return (
     <section>
       <h2>Your Recents</h2>
-      <SmallCard
-        img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
-        name="He Got A Gun"
-      />
+      <article className={styles.mostrecent}>
+        <SmallCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+        />
+        <SmallCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+        />
+        <SmallCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+        />
+        <SmallCard
+          img="https://i.scdn.co/image/ab67616d0000b2730748978f463f71da8b4486ad"
+          name="He Got A Gun"
+        />
+      </article>
     </section>
   );
 }
